a9d8437f8ba4f1fdb1628ac30b7acfb57cd7845a
Luis Donaldo Gamas <luisgamas00@gmail.com>
2024-07-03 22:20:32 -0600

    Update
    update url when scroll

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -5,17 +5,24 @@ class PageProvider extends ChangeNotifier {
 
   final List<String> _pages = ['home','about','contact','location'];
   PageController scrollController = PageController();
+  int _currentIndex = 0;
 
   createScrollController(String routeName) {
     scrollController = PageController(initialPage: getPageIndex(routeName));
+    scrollController.addListener(() {
+      final index = (scrollController.page ?? 0).round();
+      if(index != _currentIndex) {
+        html.window.history.pushState(null, 'none', '#/${_pages[index]}');
+        _currentIndex = index;
+      }
+    });
   }
 
   int getPageIndex(String routeName) {
-    return (_pages.indexOf(routeName) == -1) ? 0 : _pages.indexOf(routeName);
+    return (!_pages.contains(routeName)) ? 0 : _pages.indexOf(routeName);
   }
 
   goTo(int index) {
-    html.window.history.pushState(null, 'none', '#/${_pages[index]}');
     scrollController.animateToPage(
       index, 
       duration: const Duration(milliseconds: 300), 
